7816da27163b5b19d31b6b87d98fb57718db80b8
Matti Tahvonen <matti@vaadin.com>
2025-04-25 19:25:27 +0300

    theme draft

--- src/main/java/at/paik/AppShell.java
+++ src/main/java/at/paik/AppShell.java
@@ -2,8 +2,13 @@ package at.paik;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
 
-@PWA(name = "Paikat", shortName = "Paikat")
+@PWA(name = "Paikat", shortName = "Paikat",
+        backgroundColor = "hsl(22, 96%, 47%)",
+        themeColor = "hsl(22, 96%, 47%)"
+)
+@Theme(value = "paikatgreen", variant = "dark")
 public class AppShell implements AppShellConfigurator {
 
 }
